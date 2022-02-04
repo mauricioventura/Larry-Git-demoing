72c544191a61a12938b80104156d06dc7c161311
Mauricio Ventura <mauricio.ventura@bairesdev.com>
2022-02-04 12:09:21 -0600

    Initial Commit

---
+++ Larry Git demoing/Larry_Git_demoingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Larry_Git_demoingApp.swift
+//  Larry Git demoing
+//
+//  Created by Mauricio Ventura on 04/02/22.
+//
+
+import SwiftUI
+
+@main
+struct Larry_Git_demoingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
